c30c7193df1de9bad0529391a7b16a583f2f6e21
Mpungose David B. <DBMpungose@ers.org.sz>
2026-01-26 16:16:46 +0200

    updating app file

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ import matplotlib.pyplot as plt
 from bslib import bslib
 from shiny import reactive
 from shiny import ui, App, render, Inputs, Outputs, Session, req
-from shinywidgets import render_plotly, output_widget 
+from shinywidgets import render_plotly, output_widget
 from dictionaries.hs_dicts import sect_dict as hs_dicts
 import shinyswatch
 import io
-from shinyswatch import theme 
+from shinyswatch import theme
 
 data = pd.read_excel("C:/ERS/Statistics/Projects/Online Trade Data Platform/pract_env/prac3/data/Trade_2023_2025.xlsx"
                             , sheet_name='test2')
@@ -18,7 +18,7 @@ data = pd.read_excel("C:/ERS/Statistics/Projects/Online Trade Data Platform/prac
 assets = Path(__file__).parent / "assets"
 
 summary_page =     ui.page_fluid(
-            
+
 
     ui.layout_column_wrap(
 
@@ -31,7 +31,7 @@ summary_page =     ui.page_fluid(
                     "Total Trade",
                     ui.output_text("total_trade_value")
                 ),
-                    
+
 
                 ui.value_box(
                  "Import Value",
@@ -42,7 +42,7 @@ summary_page =     ui.page_fluid(
                     "Export Value",
                     ui.output_text("total_export")
                 ),
-                
+
                 fill= False,
     ),
     ui.br(),
@@ -53,12 +53,12 @@ summary_page =     ui.page_fluid(
                 ui.sidebar (
                     "Filter Options",
 
-                    ui.input_selectize(  
-        "monthly_partner",  
-        "Select a Country",  
+                    ui.input_selectize(
+        "monthly_partner",
+        "Select a Country",
         ['Republic of South Africa', 'Mozambique', 'India', 'Zimbabwe',
-          'Botswana', 'United Kingdom','United Arab Emirates'],  
-        multiple=True,  
+          'Botswana', 'United Kingdom','United Arab Emirates'],
+        multiple=True,
         selected='Total'
 
     ),
@@ -69,13 +69,13 @@ summary_page =     ui.page_fluid(
 
                     ,
 
-                     
+
                 ),
                          output_widget("monthly_trade")
-       
-                    
+
+
             )        ),
-         ) 
+         )
             ,
 
         ui.navset_card_tab (
@@ -91,27 +91,28 @@ summary_page =     ui.page_fluid(
                  ui.input_numeric("n_commodity", "Top # Commodities", 5, min=0, max=10),
             )
         )
-    )                        
-    
+    )
+
         ))
 
 info_page = ui.page_fluid(
-    
-    ui.layout_columns(  
-        ui.card(  
-            
+
+    ui.layout_columns(
+        ui.card(
+
                 ui.card_header (
                               ui.span("About Us", style="color: #2f2f31; font-weight: bold")
                           )
+
             ,
             ui.p("The Eswatini Revenue Service (ERS) is a semi-autonomous revenue administration agency. " \
             "It was set up through the Revenue Authority Act, 2008 (as amended). " \
             "The ERS works within the broad framework of Government but outside of the civil service." \
             "The ERS is structured" \
             " as a corporate entity and strives for operational excellence and efficiency."),
-            
-        ),  
-        ui.card(  
+
+    ),
+        ui.card(
             ui.card_header(
                 ui.span("Harmonized System (HS) Codes", style="color: #2f2f31; font-weight: bold")
             ),
@@ -121,25 +122,25 @@ info_page = ui.page_fluid(
             " numerical method of classifying traded products. It is used by countries" \
             " around the world to uniformly identify and describe products for purposes" \
             " such as assessing duties and gathering statistics."),
-            
+
         ),
-        
-          
+
+
     ),
 
     ui.layout_columns(
-        ui.card(  
-            
+        ui.card(
+
                 ui.card_header (
                               ui.span("Basic Economic Categories Rev5 (BEC)", style="color: #2f2f31; font-weight: bold")
                           )
             ,
            ui.p("The Classification by Broad Economic Categories (BEC) Revision 5 organizes goods into categories " \
            "based on their main end-use (capital, intermediate, or consumption)"),
-            
+
         ),
-        ui.card(  
-            
+        ui.card(
+
                 ui.card_header (
                               ui.span("Standard International Trade Classification Rev4 (SITC)", style="color: #2f2f31; font-weight: bold")
                           )
@@ -147,35 +148,35 @@ info_page = ui.page_fluid(
            ui.p("The Standard International Trade Classification (SITC) organizes traded products into sections, divisions, groups, and " \
            "subgroups based on materials, manufacturing stage, and industrial use. "),
         )
+    ),
     )
 
-)
 
 
 app_ui = ui.page_fluid (
 
     # ui.input_checkbox("show", value=True),
-    
-    
-    
+
+
+
     ui.include_css(Path(__file__).parent / "css/my-style.css"),
 
     ui.page_navbar(
 
-        
 
-    
+
+
 
     ui.nav_spacer(),
-    
+
     ui.nav_panel (
-        
+
         ui.span("Trade Summary",
                 style="color: white; padding-right: 10px; font-weight: bold"), summary_page),
-     
-        
 
-        ui.nav_panel ( ui.span("Trade Data",
+
+
+       ui.nav_panel ( ui.span("Trade Data",
                                style="color: white; font-weight: bold"),
           
         
@@ -218,8 +219,13 @@ ui.row (
 
             ui.input_selectize("goods_search", "Search for Commodity",
                                multiple=True,
-                               selected="Still Wine",
-                               choices=hs_dicts)
+                               selected="Textile",
+                               choices={"Textile" : "Textile", "Prepared Foodstuffs" : "Prepared Foodstuffs", 
+                                        "Mineral Products" : "Mineral Products", "Machinery and Mechanical Appliances" : "Machinery and Mechanical Appliances",
+                                        "Base Metals" : "Base Metals", "Vehicles" : "Vehicles", "Plastics" : "Plastics",
+                                        "Vegatable Products" : "Vegatable Products", "Live Animals & Products" : "Live Animals & Products",
+                                        
+                                        })
 
                       )    
                       )
@@ -268,7 +274,7 @@ ui.row (
                                style="color: white; font-weight: bold"), info_page),
           id="tabs",
           title= ui.span(
-            ui.img(src = "C:/ERS/Statistics/Projects/Online Trade Data Platform/pract_env/prac3/assets/ers logo.png", width= "40px", height= "40px"),
+            ui.img(src = "ers logo.png", width= "40px", height= "40px"),
               
               ui.span("Eswatini Merchandise Trade", style= "color: #ffcc00; padding-left: 10px"),
             ui.span(" Data Request Platform", style = "color: white")
@@ -284,35 +290,35 @@ ui.row (
           )
 
 )
-          
-            
-def server(input, output, session):    
+
+
+def server(input, output, session):
     @reactive.calc
     def total_trade():
-        agg_trade = data.groupby(['Year']).agg({'SZLValue': 'sum'}).reset_index() 
+        agg_trade = data.groupby(['Year']).agg({'SZLValue': 'sum'}).reset_index()
 
-        
+
         filtered_agg = agg_trade[agg_trade['Year'] == int(input.tradebal_filter())]
-        
+
         # if input.tradebal_filter() == 'All':
-        
+
 
 
 
         total = filtered_agg['SZLValue'].sum()
         return total
-        
+
 
     @reactive.calc
     def import_value():
-    
+
         import_data = data[data['Flow'] == 'Imports']
         year_imports = import_data[import_data['Year'] == int(input.tradebal_filter())]
         agg_imports = year_imports.groupby(['Period', 'Partner',  'HS']).agg({'SZLValue': 'sum'}).reset_index()
 
         total_import = agg_imports['SZLValue'].sum()
         return total_import
-    
+
     @reactive.calc
     def export_value():
         export_data = data[data['Flow'] == 'Exports']
@@ -320,26 +326,26 @@ def server(input, output, session):
         agg_exports = year_exports.groupby(['Period', 'Partner',  'HS']).agg({'SZLValue': 'sum'}).reset_index()
         total_export = agg_exports['SZLValue'].sum()
         return total_export
-    
+
     @reactive.calc
     def trade_filtered1():
         agg_trade = data.groupby(['Period', 'Partner', 'HS']).agg({'SZLValue': 'sum'}).reset_index()
         agg_filtered = agg_trade[agg_trade['Partner'].isin(input.partner_filter())]
         total = agg_filtered['SZLValue'].sum()
         return total
-    
+
     @render.text
     def total_trade_value():
         return f"E{total_trade():,.0f}"
-    
+
     @render.text
     def total_import():
         return f"E{import_value():,.0f}"
-    
+
     @render.text
     def total_export():
         return f"E{export_value():,.0f}"
-    
+
     @render_plotly
     def monthly_trade():
         monthly_data = data.groupby(['Month','Flow', 'Partner']).agg({'SZLValue': 'sum'}).reset_index()
@@ -353,34 +359,34 @@ def server(input, output, session):
         month_orders = calendar.month_name[1:]
         return px.bar(df, x='Month', y='SZLValue', category_orders = {'Month' : month_orders}, color="Flow"
                       , barmode='group', color_discrete_map={"Imports": "#323491", "Exports": "#ffcc00"})
-    
+
     @render_plotly
     def partner_trade():
 
 
-        
+
         patner_data = data.groupby(['Month', 'Partner', 'Flow']).agg({'SZLValue': 'sum'}).reset_index()
         partner_filtered = patner_data[patner_data['Partner'].isin(input.partner_filter())]
-        
-        
+
+
         return px.bar(partner_filtered, x='Partner', y='SZLValue', color="Flow",
                       barmode='group', color_discrete_map={"Imports": "#323491", "Exports": "#ffcc00"})
-    
+
     @render.image
     def image():
-        img = {"src": assets / "ers logo.png", "width": "60px", "height": "60px"}  
+        img = {"src": assets / "ers logo.png", "width": "60px", "height": "60px"}
         return img if input.show() else None
 
-    
+
     @render_plotly
     def plot1():
         df = data
-        
-        
+
+
         partner = df.groupby('Partner')['SZLValue'].sum().nlargest(input.n_partner()).reset_index()
 
         return px.bar(partner, x='Partner', y="SZLValue")
-    
+
     @render_plotly
     def plot2():
         df = data
@@ -390,17 +396,17 @@ def server(input, output, session):
         fig.update_xaxes(tickangle=-45)
 
         return fig
-    
+
     @reactive.calc
     def commodity_filter():
         commodity = req(input.goods_search())
-        commodity_data = data[data['HS8'].isin(commodity)]
+        commodity_data = data[data['Lib_Section'].isin(commodity)]
         return commodity_data
-    
-    
+
+
     @render.data_frame
     def trade_df():
-            
+
             commodity_df = commodity_filter()
 
 
@@ -418,7 +424,7 @@ def server(input, output, session):
 
             final_df = period_df
             return render.DataGrid(final_df,
-                                
+
                                 )
 
     @render.download(
@@ -447,21 +453,21 @@ def server(input, output, session):
 
             final_df = period_df
 
-            
+
             final_df.to_excel(buf, index=False, engine='openpyxl')
             yield buf.getvalue()
 
 
 
-    
+
 
 app = App(app_ui, server)
-    
-    
-    
-    
-            
-                
+
+
+
+
+
+
             # with ui.nav_panel("Table"):
             #     @render.table
             #     def table():
